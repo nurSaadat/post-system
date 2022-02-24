503cb5c55b04db7e8cde39721cd5b86622c57fd7
nursaadat <SNursultan@dar.kz>
2022-02-24 20:31:56 +0100

    fix(): change password

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { PostsModule } from './posts/post.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://nursaadat:ChungHa456@runtimesposts.afvax.mongodb.net/posts?retryWrites=true&w=majority',
+      'mongodb+srv://nursaadat:gegeakutama@runtimesposts.afvax.mongodb.net/posts?retryWrites=true&w=majority',
     ),
     PostsModule,
   ],
