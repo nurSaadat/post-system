3b4e6266a77598b595410f58fd80435145256209
nursaadat <SNursultan@dar.kz>
2022-02-24 06:28:47 +0100

    feat(app.controller): add route parameters

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 @Controller()
 export class PostsController {
-  @Delete()
-  delete(): string {
-    return 'delete a post';
+  @Delete(':id')
+  delete(@Param() params): string {
+    return `delete a post #${params.id}`;
   }
 
   @Post()
   create(): string {
     return 'add a new post';
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    return `return a post #${params.id}`;
   }
 
   @Get()
