96723e6f96109b7b3e97b29ffca774cef29b97c0
nursaadat <SNursultan@dar.kz>
2022-03-18 11:43:23 +0100

    fix(dto): edit dto according to definition

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,5 +1,8 @@
 export class CreatePostDto {
+  readonly status: string;
   readonly title: string;
   readonly body: string;
   readonly author: string;
+  readonly isChecked: boolean;
+  readonly isPublished: boolean;
 }
--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,6 +1,9 @@
 export class UpdatePostDto {
   readonly _id: string;
+  readonly status: string;
   readonly title: string;
   readonly body: string;
   readonly author: string;
+  readonly isChecked: boolean;
+  readonly isPublished: boolean;
 }
